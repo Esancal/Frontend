93689b5bedd317f88a89c62e3660475fe67322e4
Esancal <esteban.ca2000@outlook.cl>
2022-07-14 12:37:38 -0400

    Boton Borrar funcional

--- REACT/esancal/src/components/DatosLiComponent.js
+++ REACT/esancal/src/components/DatosLiComponent.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-const DatosLiComponent = ({ productos }) => {
+const DatosLiComponent = ({ productos, borrarProducto }) => {
   return (
     <li>
       {productos.producto} / Cantidad: {productos.cantidad} / Valor $
       {productos.valor}
       <button className="btn btn-sm btn-outline-primary me-2">Editar</button>
-      <button className="btn btn-sm btn-outline-danger">Eliminar</button>
+      <button className="btn btn-sm btn-outline-danger" onClick={()=>borrarProducto(productos.key)}>Eliminar</button>
     </li>
   );
 };
--- REACT/esancal/src/components/ListaComprasComponent.js
+++ REACT/esancal/src/components/ListaComprasComponent.js
@@ -1,61 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import DatosLiComponent from "./DatosLiComponent";
 
-const productos = [
+const initialProductos = [
   {
-    key:0,
-    producto: 'Pan',
-    cantidad: '10',
-    valor: '1500'
+    key: 0,
+    producto: "Pan",
+    cantidad: "10",
+    valor: "1500",
   },
   {
-    key:1,
-    producto: 'Arroz',
-    cantidad: '10',
-    valor: '1000'
+    key: 1,
+    producto: "Arroz",
+    cantidad: "10",
+    valor: "1000",
   },
   {
-    key:2,
-    producto: 'Cepillo de dientes',
-    cantidad: '2',
-    valor: '3000'
+    key: 2,
+    producto: "Cepillo de dientes",
+    cantidad: "2",
+    valor: "3000",
   },
   {
-    key:3,
-    producto: 'Lapices de colores',
-    cantidad: '10',
-    valor: '5000'
+    key: 3,
+    producto: "Lapices de colores",
+    cantidad: "10",
+    valor: "5000",
   },
   {
-    key:4,
-    producto: 'Shampoo 14 en 1',
-    cantidad: '1',
-    valor: '500'
+    key: 4,
+    producto: "Shampoo 14 en 1",
+    cantidad: "1",
+    valor: "500",
   },
   {
-    key:5,
-    producto: 'Lapicera',
-    cantidad: '1',
-    valor: '500'
+    key: 5,
+    producto: "Lapicera",
+    cantidad: "1",
+    valor: "500",
   },
   {
-    key:6,
-    producto: 'Esclavo',
-    cantidad: '1',
-    valor: '5000000'
-  }
-  
-]
+    key: 6,
+    producto: "Esclavo",
+    cantidad: "1",
+    valor: "5000000",
+  },
+];
 
 const ListaComprasComponent = () => {
+  const [productos, setProductos] = useState(initialProductos);
+  const borrarProducto = (productoKey) => {
+    const cambioProductos = productos.filter((p) => p.key !== productoKey);
+    setProductos(cambioProductos);
+  };
+
   return (
     <div className="col-8">
       <h1>Lista de compras</h1>
       <ul>
-      {
+        {
           /* (VARIABLE_ITERABLE => <COMPONENTE PARAMETRO = {VARIABLE_ITERABLE}/>) */
-          productos.map(p => <DatosLiComponent key={p.key} productos={p}/>)
-          }
+          productos.map((p) => (
+            <DatosLiComponent key={p.key} productos={p} borrarProducto={borrarProducto}/>
+          ))
+        }
       </ul>
     </div>
   );
